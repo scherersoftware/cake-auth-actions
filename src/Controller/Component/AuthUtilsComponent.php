1ada3943b9e86d457ff09f3116e11294243e3bd6
Michael Hoffmann <cleptric@gmail.com>
2015-04-24 11:33:59 +0200

    Added RemeberMe cookie component

--- src/Controller/Component/AuthUtilsComponent.php
+++ src/Controller/Component/AuthUtilsComponent.php
@@ -3,20 +3,42 @@ namespace AuthActions\Controller\Component;
 
 use Cake\Controller\Component;
 use Cake\Controller\Component\CookieComponent;
+use Cake\ORM\TableRegistry;
+use Cake\Utility\Hash;
 
 class AuthUtilsComponent extends Component
 {
-    public function addRemeberMeCookie()
+    public $components = ['Cookie', 'Auth', 'Flash'];
+    
+    /**
+     * Add a Remeber me cookie
+     *
+     * @param string $userId UserID
+     * @param string $options Options array for the cookie config
+     * @return void
+     */
+    public function addRemeberMeCookie($userId, $options = [])
     {
-        return;
+        $options = Hash::merge([
+            'expires' => '+14 days',
+            'httpOnly' => true,
+            'secure' => false
+        ], $options);
+        
+        $this->Cookie->config([$options]);
+        $this->Cookie->write('User.id', $userId);
     }
 
-    public function deleteRemebermeCookie()
+    /**
+     * Check if a remeber me cookie exists and login the user
+     *
+     * @return void
+     */
+    public function checkRemeberMeCookie()
     {
-        return;
-    }
-
-    private function __setupCookie() {
-        //$this->Cookie->write('name', 'Larry');
+        if (!$this->Auth->user() && $this->Cookie->read('User.id')) {
+            $user = TableRegistry::get('Users')->get($this->Cookie->read('User'))->toArray();
+            $this->Auth->setUser($user);
+        }
     }
 } 
